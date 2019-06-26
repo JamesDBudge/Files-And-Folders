0f8e6c819fcf483326e0504c63bf872fd465eb25
James Budge <thebudgie@gmail.com>
2019-06-26 09:14:45 +0100

    more stuff

--- src/main/java/com/example/filesAndFoldersHomework/components/DataLoader.java
+++ src/main/java/com/example/filesAndFoldersHomework/components/DataLoader.java
@@ -1,4 +1,31 @@
 package com.example.filesAndFoldersHomework.components;
 
-public class DataLoader {
+
+import com.example.filesAndFoldersHomework.repository.FileRepository;
+import com.example.filesAndFoldersHomework.repository.FolderRepository;
+import com.example.filesAndFoldersHomework.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    FileRepository fileRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    FolderRepository folderRepository;
+
+    public DataLoader() {
+
+    }
+
+    public void run(ApplicationArguments args) {
+        
+    }
 }
